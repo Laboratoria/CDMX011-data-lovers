072c7ced9a11a0de20e4f0d820cec9fee7589feb
fanita90 <parroquini_90@hotmail.com>
2021-07-15 19:59:26 -0500

    primeros cambios en html,css y acceder a js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,21 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+/*import { example } from './data.js';
 
-console.log(example, data);
+import data from './data/pokemon/pokemon.js';
+
+
+console.log(example, data);*/
+
+/* Leerlo con fetch
+fetch('http://localhost:5000/data/pokemon/pokemon.json')
+    .then(res => res.json())
+    .then(pokemon => {
+        console.log(pokemon.pokemon);
+
+    })
+    .catch(e => {
+        console.log("error", e)
+    })
+*/
+
+import data from './data/pokemon/pokemon.js';
+console.log(data.pokemon[0].type);//Accedemos al primer pokemon y su tipo
