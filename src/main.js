e0694379acf9960ea39a838057bc6d1d27719a58
Aliciahdzg <hernndez.alicia4@gmail.com>
2021-07-16 23:25:15 -0500

    agregado modo dinamico para obtener los tipos de pokemon

--- src/main.js
+++ src/main.js
@@ -1,14 +1,12 @@
-
-
 import data from './data/pokemon/pokemon.js';
-console.log(data.pokemon[0].name);//Accedemos al primer pokemon y su tipo
+console.log(data.pokemon[0].name); //Accedemos al primer pokemon y su tipo
 
 
 //--------------------------------Lista de type pokémon--------------------------------------
 
 
 let bloque = document.getElementById('selectorType')
-//Creando el Select que irá dentro del Div "productos"
+    //Creando el Select que irá dentro del Div "productos"
 let crearSelectType = document.createElement('select');
 let incremento = 0;
 crearSelectType.id = 'select' + incremento;
@@ -17,7 +15,7 @@ bloque.appendChild(crearSelectType);
 //Bicho, Dragón, Eléctrico, Hada, Lucha, Fuego, Volador, Fantasma, Planta, Tierra, Hielo, Normal, Veneno, Psíquico, Roca, Acero y Agua.
 
 //Llenando el arreglo que contendrá los "Options" de nuestro "Select"
-const typePokemon = [
+/*const typePokemon = [
     "Todos",
     "Fuego",
     "Agua",
@@ -36,19 +34,37 @@ const typePokemon = [
     "Acero",
     "Lucha",
     "Dragon"
-];
+];*/
+let typePokemon = new Set();
+
+data.pokemon.forEach(pokemonActual => {
+    //console.log(pokemonActual.type)
+    pokemonActual.type.forEach(tipos => {
+        //console.log(tipos);
+        typePokemon.add(tipos);
+        //console.log(pokemonType);
+    })
+})
+console.log(typePokemon);
 
 //Agregando nuestros "Options" a nuestro "Select"
-for (let i = 0; i < typePokemon.length; i++) {
-    let type = document.createElement("option");
-    incremento++;
-    type.value = typePokemon[i];
-    type.text = typePokemon[i];
-    crearSelectType.appendChild(type);
-}
-//--------------------------------------Ordenar pokémon---------------------------------------------------------
+typePokemon.forEach(tipos => {
+        let type = document.createElement("option");
+        incremento++;
+        type.value = tipos;
+        type.text = tipos;
+        crearSelectType.appendChild(type);
+    })
+    /*for (let i = 0; i < typePokemon.length; i++) {
+        let type = document.createElement("option");
+        incremento++;
+        type.value = typePokemon[i];
+        type.text = typePokemon[i];
+        crearSelectType.appendChild(type);
+    }*/
+    //--------------------------------------Ordenar pokémon---------------------------------------------------------
 let divOrden = document.getElementById('selectorOrder')
-//Creando el Select que irá dentro del Div "productos"
+    //Creando el Select que irá dentro del Div "productos"
 let crearSelectOrder = document.createElement('select');
 let incrementoOrder = 0;
 crearSelectOrder.id = 'select' + incrementoOrder;
