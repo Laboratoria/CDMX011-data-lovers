8baee0b8756a0e3efe509f57f4493bf95dd887f9
Aliciahdzg <hernndez.alicia4@gmail.com>
2021-07-26 15:07:31 -0500

    creacion funcion sort y acomodo de funcion getPokemon

--- src/data.js
+++ src/data.js
@@ -1,19 +1,19 @@
 import data from './data/pokemon/pokemon.js';
-const getPokemonTypes = () => {
-    let typePokemon = new Set(); //inicializa la variable que sera el nuevo set de datos irrepetibles
-    data.pokemon.forEach(pokemonActual => { //aqui se iteran los datos de tipo de pokemon con un forEach donde pokemonActual tiene la lista donde solo estan los tipos de pokemon,
+
+const getPokemonTypes = (filteredData) => {
+    const typePokemon = new Set(); //inicializa la variable que sera el nuevo set de datos irrepetibles
+    filteredData.forEach(pokemonActual => { //aqui se iteran los datos de tipo de pokemon con un forEach donde pokemonActual tiene la lista donde solo estan los tipos de pokemon,
         pokemonActual.type.forEach(types => { //Aqui vuelve a iterar la lista para sacar cada tipo de pokemon aunque la lista este repetida
             typePokemon.add(types); //Agrega la lista con los tipos de pokemon repetidos y creara una nueva agregando los tipos sin repetirlos
         })
     })
-    return typePokemon;
+    return typePokemon
 }
 
-const getPokemonRegion = () => {
-    let orderRegion = new Set();
-    data.pokemon.forEach(actualPokemon => {
+const getPokemonRegion = (filteredData) => {
+    const orderRegion = new Set();
+    filteredData.forEach(actualPokemon => {
         actualPokemon.generation.name;
-        //console.log(actual.generation.name)
         orderRegion.add(actualPokemon.generation.name);
     })
     return orderRegion;
@@ -37,30 +37,16 @@ const filterRegion = (pokemonActual) => {
     return resultRegion;
 }
 
-const createPokemonTypes = (pokemonActual) => {
-    let contenedor = document.querySelector("#containerPokemon");
-    let mostrarPokemon = document.createElement("div");
-    mostrarPokemon.setAttribute("class", "pokeDiv");
-    //console.log("la clase del pokemon es:", mostrarPokemon);
-    contenedor.appendChild(mostrarPokemon);
+const sortData = (filteredData, userOption) => {
+    if (userOption == "Orden ascendente") {
+        filteredData.sort(function(a, b) { return a - b });
+    } else if (userOption == "Orden descendente") {
+        filteredData.reverse();
+    }
 
-    let mostrarImg = document.createElement("img");
-    mostrarImg.src = pokemonActual.img;
-    mostrarImg.setAttribute("class", "pokeImg");
-    //mostrarImg.className = "pokeImg";
-    //console.log(mostrarImg);
-    mostrarPokemon.appendChild(mostrarImg);
+    return filteredData;
+}
 
-    let tituloImg = document.createElement("h4");
-    let namePokemon = pokemonActual.name;
-    let initialName = namePokemon.charAt(0).toUpperCase();
-    let restName = namePokemon.slice(1);
-    let resultName = initialName + restName;
-    /*Mostramos el número + nombre en pantalla*/
-    tituloImg.innerHTML = pokemonActual.num + " " + resultName;
-    mostrarPokemon.appendChild(tituloImg);
-    //console.log(contenedor);
 
-};
 
-export { getPokemonTypes, getPokemonRegion, filterTypes, filterRegion, createPokemonTypes };
+export { getPokemonRegion, filterTypes, filterRegion, getPokemonTypes, sortData };
--- src/main.js
+++ src/main.js
@@ -1,31 +1,66 @@
 import data from './data/pokemon/pokemon.js';
-
 import {
     getPokemonTypes,
     filterTypes,
-    createPokemonTypes,
     getPokemonRegion,
     filterRegion,
+    sortData,
 } from './data.js';
 //import { types } from '@babel/core';
 
+let filteredData = data.pokemon;
+//----------------- Creacion de div para mostrar los pokemon en la pagina inicial------------------
+const createPokemonTypes = (dataPokemon) => {
+    let contenedor = document.querySelector("#containerPokemon");
+    let mostrarPokemon = document.createElement("div");
+    mostrarPokemon.setAttribute("class", "pokeDiv");
+    contenedor.appendChild(mostrarPokemon);
 
+    let mostrarImg = document.createElement("img");
+    mostrarImg.src = dataPokemon.img;
+    mostrarImg.setAttribute("class", "pokeImg");
+    //mostrarImg.className = "pokeImg";
+    mostrarPokemon.appendChild(mostrarImg);
+
+    let tituloImg = document.createElement("h4");
+    let namePokemon = dataPokemon.name;
+    let initialName = namePokemon.charAt(0).toUpperCase();
+    let restName = namePokemon.slice(1);
+    let resultName = initialName + restName;
+    tituloImg.innerHTML = dataPokemon.num + " " + resultName; //Mostramos el número + nombre en pantalla
+    mostrarPokemon.appendChild(tituloImg);
+};
 //--------------------------------Creación de lista Type pokémon--------------------------------------
 
 let selectorType = document.getElementById('selectorType')
-let crearSelectType = document.createElement('select'); //Creando el Select que irá dentro del Div "productos"
-selectorType.appendChild(crearSelectType);
+let createSelectType = document.createElement('select'); //Creando el Select que irá dentro del Div "productos"
+selectorType.appendChild(createSelectType);
 const firstOption = document.createElement("option");
 firstOption.value = "Todos los Pokemon";
 firstOption.text = "Todos los Pokemon";
-crearSelectType.appendChild(firstOption);
+createSelectType.appendChild(firstOption);
 
-let pokemonTypes = getPokemonTypes() //Agregando nuestros "Options" a nuestro "Select"
+
+let pokemonTypes = getPokemonTypes(filteredData) //Agregando nuestros "Options" a nuestro "Select"
 pokemonTypes.forEach(types => { //Otro forEach a los tipos de pokemon para iterar la lista, despues crea el elemento
     let type = document.createElement("option");
     type.value = types;
     type.text = types;
-    crearSelectType.appendChild(type);
+    createSelectType.appendChild(type);
+})
+document.getElementById("selectorType").addEventListener("change", (event) => {
+    //location.reload();
+    if (event.target.value == "Todos los Pokemon") {
+        location.reload()
+    } else {
+        filteredData = filterTypes(event.target.value);
+        document.getElementById("containerPokemon").innerHTML = "";
+
+        filteredData.forEach(pokemonActual => {
+            createPokemonTypes(pokemonActual);
+        });
+    }
+
 })
 
 //--------------------------------------Creación de Ordenar pokémon por generación---------------------------------------------------------
@@ -33,37 +68,48 @@ let selectorRegion = document.getElementById("selectorRegion")
 let createSelectRegion = document.createElement('select'); //Creando el Select que irá dentro del Div "productos"
 selectorRegion.appendChild(createSelectRegion);
 
-let pokemonRegion = getPokemonRegion()
+let pokemonRegion = getPokemonRegion(filteredData)
     //Agregando nuestros "Options" a nuestro "Select"
 pokemonRegion.forEach(regionActual => {
-        let region = document.createElement("option");
-        region.value = regionActual;
-        region.text = regionActual;
-        createSelectRegion.appendChild(region);
+    let region = document.createElement("option");
+    region.value = regionActual;
+    region.text = regionActual;
+    createSelectRegion.appendChild(region);
+})
+document.getElementById("selectorRegion").addEventListener("change", (event) => {
+        //location.reload();
+        filteredData = filterRegion(event.target.value);
+        document.getElementById("containerPokemon").innerHTML = "";
+
+        filteredData.forEach(pokemonActual => {
+            createPokemonTypes(pokemonActual);
+        });
+
     })
-    //-------------------------Mostrar Pokemon---------------------------------
-    //let showPokemon = data.pokemon.forEach()
-data.pokemon.forEach(pokemonActual => {
-    const showPokemon = createPokemonTypes(pokemonActual);
+    //-------------------------Mostrar Pokemon--------------------------------
+
+filteredData.forEach(pokemonActual => {
+    createPokemonTypes(pokemonActual);
 
 });
+//---------------------Ordenar Ascendente-Descendente-------------------
+let selectorSort = document.getElementById("selectorSort")
+let createSelectSort = document.createElement('select'); //Creando el Select que irá dentro del Div "productos"
+selectorSort.appendChild(createSelectSort);
+const sortAscending = document.createElement("option");
+sortAscending.value = "Orden ascendente";
+sortAscending.text = "Orden ascendente";
+createSelectSort.appendChild(sortAscending);
+const sortDescending = document.createElement("option");
+sortDescending.value = "Orden descendente";
+sortDescending.text = "Orden descendente";
+createSelectSort.appendChild(sortDescending);
 
-document.getElementById("selectorType").addEventListener("change", (event) => {
-    let data2 = filterTypes(event.target.value)
-    document.getElementById("containerPokemon").innerHTML = ""
-
-    data2.forEach(pokemonActual => {
-        const showPokemon = createPokemonTypes(pokemonActual);
+document.getElementById("selectorSort").addEventListener("change", (event) => {
+    const userOption = event.target.value;
+    filteredData = sortData(filteredData, userOption);
+    document.getElementById("containerPokemon").innerHTML = "";
+    filteredData.forEach(pokemonActual => {
+        createPokemonTypes(pokemonActual);
     });
-
-})
-
-document.getElementById("selectorRegion").addEventListener("change", (event) => {
-    let data2 = filterRegion(event.target.value)
-    document.getElementById("containerPokemon").innerHTML = ""
-
-    data2.forEach(pokemonActual => {
-        const showPokemon = createPokemonTypes(pokemonActual);
-    });
-
 })
