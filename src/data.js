27ce6e916191ee2eedc8ba4536e793c7635d2f3f
Aleisi <86322128+Aleisi@users.noreply.github.com>
2021-08-10 10:43:28 -0500

    guardando cambios Aleisi

--- src/data.js
+++ src/data.js
@@ -8,7 +8,6 @@ export const anotherExample = () => {
     return 'OMG';
 };
 
-
 /*---Posible filtro para obtener tipos de pokemones---
 let allPokemon = [{data.pokemon}];
 let pokemonType == type.filter(function(type){
@@ -17,13 +16,30 @@ console.log(pokemon-type);
 })
 */
 
-/*import data from './data/pokemon/pokemon.js';
-console.log()*/
 
-export function filterByType(dataPokemon, pokemonType){
-    let result = dataPokemon.filter(function(dataPokemon){
-                    return dataPokemon.type == pokemonType;
+/*La función filterByType recibe en los () los datos del pokemon.js y rebibe los tipos de pokemon (pokemonType)
+ejecutamos el filtrado de los pokemones en dataPokemon.filter y nos retornará un resultado, ese resultado lo 
+declaramos guardandolo en la variable let result y la retornamos para que nos de el filtrado
+filterByType espera todos los pokemones y despues el tipo
+
+pokemonType lo reutilizamos 
+*/
+
+/*export function filterByType(dataPokemon, pokemonType) {
+    let result = dataPokemon.filter(function (dataPokemon) {
+        return dataPokemon.type == pokemonType;
     })
     return result
+}*/
+
+export function filterByType(dataPokemon, pokemonType) {
+    let result = dataPokemon.filter((pokemons) => pokemons.type == pokemonType);
+    return result;
 }
 
+/*export function filterByGeneration(dataPokemon, pokemonGeneration) {
+    let result = dataPokemon.filter(function (dataPokemon) {
+        return dataPokemon.generation.name == pokemonGeneration;
+    });
+    return result;
+}*/
--- src/main.js
+++ src/main.js
@@ -1,29 +1,68 @@
 /*Manipulación del DOM*/
-import data from './data/pokemon/pokemon.js';
-import { filterByType } from './data.js';
+import data from "./data/pokemon/pokemon.js";
+import { filterByType } from "./data.js";
 
 const dataPokemon = data.pokemon;
-console.log(dataPokemon);
 
-/*data.forEach()
-innerHTML*/
-let primeraHistoria = "" 
-dataPokemon.forEach(cadaPokemon =>{
-primeraHistoria += primeraPantalla (cadaPokemon)
-})
-console.log(primeraHistoria);
+// Primera pantalla donde muestra todos los pokemones
+let pokemonHtml = "";
 
-function primeraPantalla (categoriaPokemon){
-    return `<div class = "box">
-    <h3>NOMBRE:${categoriaPokemon.name}</h3>
-    <p>NÚMERO:${categoriaPokemon.num}</p>
-    <p>PESO:${categoriaPokemon.size.weight}</p>
-    <p>ALTURA:${categoriaPokemon.size.height}</p>
-    <img src="${categoriaPokemon.img}">
+dataPokemon.forEach((eachPokemon) => {
+  pokemonHtml += `<div class = "box">
+    <h3>NOMBRE:${eachPokemon.name}</h3>
+    <p>NÚMERO:${eachPokemon.num}</p>
+    <p>PESO:${eachPokemon.size.weight}</p>
+    <p>ALTURA:${eachPokemon.size.height}</p>
+    <img src="${eachPokemon.img}">
+    </div>`;
+});
+//parent element
+document.getElementById("root").innerHTML = pokemonHtml;
+
+//console.log(filterByType(dataPokemon, "poison"))
+
+//data.forEach
+//innerHTML
+//Añadimos un select y accedemos a el, le ponemos un addEventListener que detecte los cambios con el change, una vez que detecta el cambio ejecuta las funciones
+//Detectar el cambio del select con un addEventListener (que detecte los cambios con el change) change  y ejecutar la función filtrado
+//como el onclick pero aqui sera el change
+//Ejecutar filtrado por tipo pero en lugar de fire poner evento que mandamos
+//mediante event.target.value, para acceder al valor cuando le hacemos un cambio a select
+//nos dará acceso al valor cuando hacemos un cambio en select
+
+document.getElementById("type").addEventListener("change", function () {
+  const typeHtml = document.getElementById("type").value;
+  const pokemonType = filterByType(dataPokemon, typeHtml);
+
+  let showfilter = "";
+  pokemonType.forEach((filter) => {
+    showfilter += `<div class = "box">
+        <h3>NOMBRE:${filter.name}</h3>
+        <p>NÚMERO:${filter.num}</p>
+        <p>PESO:${filter.size.weight}</p>
+        <p>ALTURA:${filter.size.height}</p>
+        <img src="${filter.img}">
+        </div>`;
+  });
+
+  document.getElementById("root").innerHTML = showfilter;
+});
+
+/*const selectType = document.querySelector('.filter')
+
+selectType.addEventListener('change', (event) => {
+    const pokemonFilter = filterByType(dataPokemon,event.target.value);
+    const typePokemon = pokemonFilter.map((categoryPokemon) =>
+
+    `<div class = "box">
+    <h3>NOMBRE:${categoryPokemon.name}</h3>
+    <p>NÚMERO:${categoryPokemon.num}</p>
+    <p>PESO:${categoryPokemon.size.weight}</p>
+    <p>ALTURA:${categoryPokemon.size.height}</p>
+    <img src="${categoryPokemon.img}">
     </div>`
-}
+    )
 
-document.getElementById("root").innerHTML=primeraHistoria
+    document.getElementById("root").innerHTML=typePokemon
 
-console.log(filterByType(dataPokemon,"fire"))
-
+});*/
